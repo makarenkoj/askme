f6cf23d9fff8c00b3875556e3e8d8bfc681f2234
makarenkoj <makarenkoj53@gmail.com>
2019-10-27 14:53:47 +0200

    lesson 50

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,51 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Yurii',
+        username: 'makarenkoj',
+        avatar_url: 'https://super-avatara.ru/images/trueimg/pictures/69/F2546A44EA1E-69.gif'
+        ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'aristofun'
+        ),
+      User.new(
+        id: 3,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+        )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = "Привет, Юра!"
+  	@user = User.new(
+  		name: 'Yurii',
+  		username: 'makarenkoj',
+      avatar_url: 'https://super-avatara.ru/images/trueimg/pictures/69/F2546A44EA1E-69.gif'
+  		)
+
+    @questions = [
+      Question.new(
+        text: 'Как дела?', created_at: Date.parse('19.10.2019')
+        ),
+      Question.new(
+        text: 'В чем смысл жизни?', created_at: Date.parse('19.10.2019')
+      ),
+       Question.new(
+        text: 'Почём опиум для народа?', created_at: Date.parse('19.10.2019')
+      )
+    ]
+
+    @new_question = Question.new
   end
 end
