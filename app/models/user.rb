65125ebd20b835d71303efaa05e0850f3f9c622e
makarenkoj <makarenkoj53@gmail.com>
2019-11-15 00:59:23 +0200

    update validation background

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,8 @@ class User < ApplicationRecord
   validates :password, presence: true, on: :create
   validates_confirmation_of :password
 
-  validates :background_color, format: { with: /\A\#([a-fA-F]|[0-9]){3,6}\z/ }
+  validates :background_color, format: { with: /\A\#([a-fA-F]|[0-9]){3,6}\z/ },
+            on: :update
 
   before_validation :downcase_username
   before_save :encrypt_password
