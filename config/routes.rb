98b1636914e2ef5013577736875b1446dcf8d3fa
makarenkoj <makarenkoj53@gmail.com>
2019-12-21 12:00:48 +0200

    security route

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,9 @@ Rails.application.routes.draw do
   resources :users
 
   # Ресурс сессий (только три экшена :new, :create, :destroy)
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   # Ресурс вопросов (кроме экшенов :show, :new, :index)
   resources :questions, except: [:show, :new, :index]
 
-  # Синонимы путей — в дополнение к созданным в ресурсах выше.
-  #
-  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
-  # их удалим.
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
 end
